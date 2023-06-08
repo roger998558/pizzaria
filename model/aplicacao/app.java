516671963f0c22552fdf4500a70a40ac64bea441
roger998558 <cezarroger71@gmail.com>
2023-06-07 22:05:29 -0300

    commit pizza

--- model/aplicacao/app.java
+++ model/aplicacao/app.java
@@ -1,5 +1,12 @@
 package model.aplicacao;
 
+import model.pizza;
+
 public class app {
-    
+    public static void main(String[] args) {
+        
+        pizza pedido=new pizza("sabor");
+        System.out.println("pizza criada");
+        System.out.println(pedido.pedidoifood());
+    }
 }
